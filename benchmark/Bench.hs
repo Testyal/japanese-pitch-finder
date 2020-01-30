3d2447fbeb4d013d5131b88daa3c02cb68f0b1ca
Billy <williamvrossii@gmail.com>
2020-01-30 11:31:06 +0000

    Updated Bench.hs to reflect previous changes

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,10 +1,24 @@
 import qualified Data.ByteString.Lazy as BL
 import Criterion.Main
-import CityRecyclingAndCodeWasteCenter.MatchOld
-import CityRecyclingAndCodeWasteCenter.SearchOld
+import qualified Data.Vector as V
+import Dictionary.Csv
+import Dictionary.Search
+import Dictionary.Types
+
+sampleEntries :: [String]
+sampleEntries = ["映画", "音楽", "雑誌", "朝ご飯", "お酒", "お茶", "水", "家", "読む", "起きる","全然","勉強する"]
+
+benchSingleEntry :: Dictionary -> String -> Benchmark 
+benchSingleEntry dict entry = bench ("Dictionary.Search.filterEntries for the entry " ++ entry) $ whnf (filterEntries dict) entry
 
 main :: IO ()
 main = do
-    dict <- BL.readFile "data/WaDokuDa.tsv"
-    defaultMain [bench "convertCsvToDictionary" $ whnf convertCsvToDictionary dict]
-    putStrLn "very fast :)"
+    wadoku <- BL.readFile "data/NewWaDokuDa.tsv"
+    --defaultMain [bench "Dictionary.Csv.convertCsvToDictionary" $ whnf convertCsvToDictionary wadoku]
+    putStrLn "Preparing to benchmark the search phase..."
+    case convertCsvToDictionary wadoku of
+        Left err -> putStrLn err 
+        Right dict -> do
+            putStrLn "The documentation of the vector package indicates Vector.filter is (not unexpectedly) O(n)."
+            putStrLn $ "The WaDoku dictionary has length " ++ show (V.length dict) ++ "."
+            defaultMain $ map (benchSingleEntry dict) sampleEntries
